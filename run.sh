748fc85088ff9c2014cf83d1b4e36191d8dcadd5
adhofer <a1209967@univie.ac.at>
2022-03-05 11:19:16 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+python main.py --config-file configs/faster_rcnn_res101.yaml
+python main.py --config-file configs/sgg_res101_step.yaml
+az vm deallocate -g STUD_INF_Hofer_Adrian -n imagefact-graph-rcnn
